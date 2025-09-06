7b12a1717626c24430b094d02238baab1f229d9d
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-09-07 02:49:14 +0530

    polarFix

--- backend/configs/polarConfig.js
+++ backend/configs/polarConfig.js
@@ -1,8 +1,8 @@
 const { Polar } = require("@polar-sh/sdk");
 
 const polar = new Polar({
-  accessToken: process.env.POLAR_ACCESS_TOKEN_DEV, // store in .env
-  server: 'sandbox', //TODO: remove or change to 'production' later
+  accessToken: process.env.POLAR_ACCESS_TOKEN, // store in .env
+  server: process.env.NODE_ENV == "production" ? "production" :'sandbox',
 });
 
 module.exports = polar
