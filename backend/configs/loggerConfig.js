10a9abd6af60bae3134b81dcd4e8b1c2bd676e75
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-09-07 13:41:33 +0530

    Removed Loki

--- backend/configs/loggerConfig.js
+++ backend/configs/loggerConfig.js
@@ -1,32 +1,18 @@
 const { createLogger, format, transports } = require("winston");
-const LokiTransport = require('winston-loki');
 const { colorizeLevel, colorizeRequest } = require("../services/colorService");
 
 /** * Levels and Priority error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6 */
 
 const logger = createLogger({
-  level: process.env.NODE_ENV !== "production" ? "silly" :" http", // accept all levels >= silly on Development
+  level: process.env.NODE_ENV !== "production" ? "silly" :"http", // accept all levels >= silly on Development
   format: format.combine(
     format.timestamp(),
     format.errors({ stack: true }), // ensures error stack traces are logged
     format.splat(),
     format.json() // base format for Loki
   ),
-  transports:[
-    new transports.File({ filename: "logs/error.log", level: "error" }),
-    new transports.File({ filename: "logs/combined.log" })
-  ]
+  transports:[]
 });
-
-logger.add(new LokiTransport({
-  level: "http",
-  labels: { app: "express", service: "querynox_backend" },
-  host: process.env.LOKI_LOGGER_HOST,
-  basicAuth: `${process.env.LOKI_USER}:${process.env.LOKI_API_KEY}`,
-  batching: true,
-  interval: 5,
-  json: true
-}));
 
 // Custom console formatter
 const customConsoleFormatter = format.printf(({ level, message, timestamp, ...rest }) => {
@@ -46,6 +32,11 @@ if (process.env.NODE_ENV !== "production") {
       format.timestamp(),
       customConsoleFormatter
     )
+  }));
+}else{
+  logger.add(new transports.Console({
+    level: "http", // log everything to console
+    format: format.json()
   }));
 }
 
